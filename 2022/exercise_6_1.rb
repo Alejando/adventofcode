63329f8c6e204b1ae8b4f35a0b57b704ca3c7941
Alejandro Prado <alejandro.prado@konta.com>
2022-12-13 09:24:38 -0600

    Exercise 6 2022

---
+++ 2022/exercise_6_1.rb
@@ -0,0 +1,14 @@
+input = File.read("#{File.dirname(__FILE__)}/exercise_6.txt")
+start_point = 0
+end_point = 3
+
+while end_point < input.size
+  substring = input[start_point..end_point]
+  if substring.chars.uniq.count == substring.length
+    puts end_point + 1
+    return
+  else
+    start_point += 1
+    end_point += 1
+  end
+end
